3c1bc643e3d18dc973cfab6a5ffbb679c24995bb
PC_HOME <narin.tent.95@gmail.com>
2022-09-02 03:35:23 +0700

    add lib

---
+++ printf/libft/ft_atol.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwattana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 01:43:58 by nwattana          #+#    #+#             */
+/*   Updated: 2022/08/18 23:36:16 by nwattana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *src)
+{
+	long	res;
+	long	minus;
+	char	*c;
+
+	c = (char *)src;
+	res = 0;
+	minus = 1;
+	while (ft_isspace(*c))
+		c++;
+	if (*c == '+' || *c == '-')
+	{
+		if (*c == '-')
+		{
+			minus *= -1;
+		}
+		c++;
+	}
+	while (ft_isdigit(*c))
+	{
+		res *= 10;
+		res += (*c) - '0';
+		c++;
+	}
+	return (res * minus);
+}
